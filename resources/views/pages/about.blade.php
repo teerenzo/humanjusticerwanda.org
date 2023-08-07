92543d4486140c269bfcbf39fffbf2b33734e4c4
Renzaho Emmanuel <71966667+teerenzo@users.noreply.github.com>
2023-08-07 22:51:22 +0200

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+        <title>Human justices organisation</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
